34226ed52f502fc0c36097e2476637fc129ca9a1
songsogu@gmail.com <songsogu@gmail.com>
2019-09-22 02:56:32 +0900

    스프링 MVC마지막 커밋 @Controller @RequestMapping @Repository @Autowired 등등

---
+++ lec16pjt001/src/main/java/spring/lec16pjt001/Lec16pjt001Application.java
@@ -0,0 +1,13 @@
+package spring.lec16pjt001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec16pjt001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lec16pjt001Application.class, args);
+    }
+
+}
